b80d93f776b441c82a1dc728a96a92010bef8886
YJJUNG <fltk0601@naver.com>
2023-05-18 01:46:42 +0900

    백준 9012_스택방식

--- src/coding/code_9012_2.java
+++ src/coding/code_9012_2.java
@@ -3,8 +3,9 @@ package coding;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class code_9012 {
+public class code_9012_2 {
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static void main(String[] args) throws IOException {
 		int testCase = 0;
@@ -13,31 +14,36 @@ public class code_9012 {
 	    for (int i = 0; i < testCase; i++) {
 			s = br.readLine();
 			int size = s.length();
-			int length = 0;
+			Stack<String> str = new Stack<String>();
 			for(int j = 0; j < size; j++) {
 				if(size%2 != 0) {
-					length = -1;
 					break;
 				} else {
-					if(length >= 0) {
-						if("(".equals(Character.toString(s.charAt(j)))) {
-							length++;
-						}else if(")".equals(Character.toString(s.charAt(j)))) {
-							length--;
+					if("(".equals(Character.toString(s.charAt(j)))) {
+						str.push(Character.toString(s.charAt(j)));
+					}else if(")".equals(Character.toString(s.charAt(j)))) {
+						if(str.size() > 0) {
+							str.pop();
 						}else {
-							length = -1;
+							str.push(Character.toString(s.charAt(j)));
 							break;
 						}
 					}else {
-						
+						str.push(Character.toString(s.charAt(j)));
+						break;
 					}	
 				}
 			}
-			if(length == 0) {
-				System.out.println("YES");
-			}else {
+			if(size%2 != 0) {
 				System.out.println("NO");
+			}else {
+				if(str.isEmpty()) {
+					System.out.println("YES");
+				}else {
+					System.out.println("NO");
+				}
 			}
+			str.clear();
 	    } 
 	}
 }
